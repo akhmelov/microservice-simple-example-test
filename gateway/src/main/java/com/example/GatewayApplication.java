c29c0d6bb0ae5d976e6d66ec6a87ba3bec0ced44
akhmelov <akhmelow@gmail.com>
2016-07-26 15:43:49 +0200

    gateway learning

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -2,10 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
